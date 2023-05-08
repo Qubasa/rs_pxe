6eda7b48d3e62ec09b8e5c0d4819a75a7b48dda2
Luis-Hebendanz <luis.nixos@gmail.com>
2023-05-08 02:22:41 +0200

    Fix build fail

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
           src = buildDir;
           cargoLock.lockFile = ./Cargo.lock;
           pname = "pxe-rs";
-          nativeBuildInputs = [ ];
+          nativeBuildInputs = [ pkgs.breakpointHook ];
           version = "0.1.0";
         };
 
