dafaf36f2e47807ead604889cb4e3a70384e5c62
Luis-Hebendanz <luis.nixos@gmail.com>
2023-06-22 21:07:49 +0200

    Added ipxe.dsk

--- run.sh
+++ run.sh
@@ -7,34 +7,38 @@ set -x
 LAN=enp2s0
 BRIDGE=kmania_br0
 TAPIF=kmania_tap0
-
+TAPIF2=kmania_tap1
 
 function reset_net {
-  sudo dhcpcd -k $BRIDGE
+  #sudo dhcpcd -k $BRIDGE
   sudo ip link set "$TAPIF" down
+  sudo ip link set "$TAPIF2" down
   sudo ip link set $BRIDGE down
   sudo brctl delif $BRIDGE "$TAPIF"
+  sudo brctl delif $BRIDGE "$TAPIF2"
   sudo brctl delbr $BRIDGE
   sudo ip tuntap del dev "$TAPIF" mode tap
+  sudo ip tuntap del dev "$TAPIF2" mode tap
 }
 function ctrl_c() {
     echo "** Trapped CTRL-C"
-    #reset_net
     pkill rs_pxe || true
+    reset_net
 }
 function setup_net {
   sudo ip tuntap add dev "$TAPIF" mode tap user "$USER"
+   sudo ip tuntap add dev "$TAPIF2" mode tap user "$USER"
   sudo brctl addbr $BRIDGE &> /dev/null
-  sudo brctl addif $BRIDGE $LAN &> /dev/null
+  sudo brctl addif $BRIDGE $TAPIF2 &> /dev/null
   sudo brctl addif $BRIDGE "$TAPIF"
   sudo ip link set $BRIDGE up
-  sudo ip link set $LAN up
+  sudo ip link set $TAPIF2 up
   sudo ip link set "$TAPIF" up
-  sudo dhcpcd -n $BRIDGE
+  #sudo dhcpcd -n $BRIDGE
 }
 
 trap ctrl_c INT
-#setup_net
+setup_net
 
 PAYLOAD=$(cat <<EOF
 set -xe
@@ -42,8 +46,8 @@ export RUST_BACKTRACE=1
 pkill rs_pxe || true
 cargo build
 sudo setcap cap_net_admin,cap_net_raw=eip ./target/debug/rs_pxe
-./target/debug/rs_pxe --raw "$LAN"  --mac "98:fa:9b:4b:b2:c4" --ip "192.168.178.96" & #  --mac "18:70:75:7B:3F:FE"  
-#qemu-system-x86_64 -enable-kvm -m 1024 -net nic -net tap,ifname="$TAPIF",script=no,downscript=no -cdrom ipxe.iso -serial stdio -display none 
+./target/debug/rs_pxe --raw "$TAPIF2"  --mac "98:fa:9b:4b:b2:c4" --ip "192.168.178.96" & #  --mac "18:70:75:7B:3F:FE"  
+qemu-system-x86_64 -enable-kvm -m 1024 -net nic -net tap,ifname="$TAPIF",script=no,downscript=no -fda ipxe.dsk -snapshot -serial stdio -display none 
 EOF
 )
 
